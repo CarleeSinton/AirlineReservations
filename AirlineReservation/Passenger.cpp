0d111d110c33429be0b55ca365e2a0bba2379aec
CarleeSinton <carleen.sinton@gmail.com>
2019-08-29 16:04:21 -0700

    Defining and Implementing Passenger
    This is the beginning of defining and implementing passenger - this will allow the user to add a passenger

---
+++ AirlineReservation/Passenger.cpp
@@ -0,0 +1,33 @@
+#include "Passenger.h"
+
+using namespace std;
+
+namespace AirlineApp {
+	Passenger::Passenger(const string& firstName, const string& lastName):
+		mFirstName(firstName),mLastName(lastName)
+	{ }
+
+	void Passenger::setFirstName(const string& firstName) {
+		mFirstName = firstName;
+	}
+
+	const string& Passenger::getFirstName() const {
+		return mFirstName;
+	}
+
+	void Passenger::setLastName(const string& lastName) {
+		mLastName = lastName;
+	}
+
+	const string& Passenger::getLastName() const {
+		return mLastName;
+	}
+	
+	void Passenger::setTicketNumber(int ticketNumber) {
+		mTicketNumber = ticketNumber;
+	}
+
+	int Passenger::getTicketNumber() const {
+		return mTicketNumber;
+	}
+}
---
+++ AirlineReservation/Passenger.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <string>
+
+//This is definition of a passenger
+namespace AirlineApp {
+
+	class Passenger {
+	public:
+		Passenger() = default;
+		Passenger(const std::string& firstName, const std::string& lastName);
+
+		void displayPassenger() const;
+
+		void setFirstName(const std::string& firstName);
+		const std::string& getFirstName() const;
+
+		void setLastName(const std::string& lastName);
+		const std::string& getLastName() const;
+
+		void setTicketNumber(int ticketNumber);
+		int getTicketNumber() const;
+
+
+	private:
+		std::string mFirstName;
+		std::string mLastName;
+		int mTicketNumber = 1;
+	};
+}
